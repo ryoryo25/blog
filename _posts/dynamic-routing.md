2f2988b1a51bbfe8ecd62fe7b5ce6e5c0db6c944
ry_Tom <ryotoyota@outlook.jp>
2023-09-15 15:17:09 +0900

    add update date

--- _posts/dynamic-routing.md
+++ _posts/dynamic-routing.md
@@ -2,7 +2,9 @@
 title: 'Dynamic Routing and Static Generation'
 excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.'
 coverImage: '/assets/blog/dynamic-routing/cover.jpg'
-date: '2020-03-16T05:35:07.322Z'
+dates:
+  postDate: '2020-03-16T05:35:07.322Z'
+  updateDate: '2020-04-16T05:35:07.322Z'
 author:
   name: JJ Kasper
   picture: '/assets/blog/authors/jj.jpeg'
--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -1,12 +1,25 @@
 import { parseISO, format } from 'date-fns'
+import Dates from '../interfaces/dates'
 
 type Props = {
-  dateString: string
+  dates: Dates
 }
 
-const DateFormatter = ({ dateString }: Props) => {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+const DateFormatter = ({ dates }: Props) => {
+  const postDate = parseISO(dates.postDate)
+  const postDateHTML = <time dateTime={dates.postDate}>{format(postDate, 'LLLL	d, yyyy')}</time>
+
+  if (dates.updateDate === '') {
+    return postDateHTML
+  } else {
+    const updateDate = parseISO(dates.updateDate)
+    return (
+      <>
+        {postDateHTML}<br/>
+        <time dateTime={dates.updateDate}>{format(updateDate, 'LLLL	d, yyyy')}</time>
+      </>
+      )
+  }
 }
 
 export default DateFormatter
