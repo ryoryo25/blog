19ab1baea8332d77d75d1dc3098dbfe8284e4f8c
ry_Tom <ryotoyota@outlook.jp>
2023-09-20 15:28:57 +0900

    add own favicon

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { HOME_OG_IMAGE_URL } from '../lib/constants'
+import { BLOG_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
 
 const Meta = () => {
   return (
@@ -25,16 +25,16 @@ const Meta = () => {
       <link
         rel="mask-icon"
         href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
+        color="#5bbad5"
       />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
+      <meta name="theme-color" content="#ffffff" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js and.`}
+        content={BLOG_NAME}
       />
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
