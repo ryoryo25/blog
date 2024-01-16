a75d0b6678e3bfcd6a9520a019771ff24c362ca4
ry_Tom <ryotoyota@outlook.jp>
2024-01-16 12:34:57 +0900

    use dangerouslySetInnerHTML

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -1,8 +1,3 @@
-import Script from "next/script"
-import { pageview } from "../lib/gtag"
-import { useEffect } from "react"
-import { useRouter } from "next/router"
-
 type Props = {
   gaId: string
 }
@@ -11,16 +6,17 @@ export default function GoogleAnalytics({ gaId }: Props) {
   return (
     <>
       {/* <!-- Google tag (gtag.js) --> */}
-      <Script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} />
-      <Script id="gtag-init" strategy="afterInteractive">
-        {`
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} />
+      <script
+        id="gtag-init"
+        dangerouslySetInnerHTML={{
+          __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag() { dataLayer.push(arguments); }
-          gtag('js', new Date());
+          gtag(\'js\', new Date());
 
-          gtag('config', '${gaId}');
-        `}
-      </Script>
+          gtag(\'config\', \'${gaId}\');
+        ` }} />
     </>
   )
 }
