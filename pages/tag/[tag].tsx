b357dda0b1f29ef0bcc83b43cffa6d210ec03e42
ry_Tom <ryotoyota@outlook.jp>
2024-10-23 00:46:40 +0900

    complete tagged posts page

---
+++ pages/tag/[tag].tsx
@@ -0,0 +1,85 @@
+import { BLOG_NAME, INITIAL_PAGE_NUMBER, POSTS_PER_PAGE } from "@/lib/constants"
+import Layout from "@/components/layout"
+import Head from "next/head"
+import Container from "@/components/container"
+import ListPosts from "@/components/list-posts"
+import Pagination from "@/components/pagination"
+import type Post from "@/interfaces/post"
+import { PostEntry } from "@/interfaces/post"
+import { getAllPosts, paginationRange } from "@/lib/api"
+import TagTitle from "@/components/tag-title"
+
+type Props = {
+  posts: Post[]
+  pages
+  tag: string
+}
+
+export default function Tag({ posts, pages, tag }: Props) {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>{`${BLOG_NAME} | Tag: ${tag}`}</title>
+        </Head>
+        <Container>
+            <TagTitle tag={tag} />
+            {posts.length > 0 && <ListPosts posts={posts} />}
+            <Pagination
+              pages={pages}
+              currentPage={INITIAL_PAGE_NUMBER}
+              basePath={`/tag/${tag}`}
+            />
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+type Params = {
+  params: {
+    tag: string
+  }
+}
+
+export async function getStaticProps({ params }: Params) {
+  const posts = getAllPosts([
+    PostEntry.TITLE,
+    PostEntry.DATES,
+    PostEntry.TAGS,
+    PostEntry.SLUG,
+    PostEntry.COVER_IMAGE,
+  ]).filter(post => post.tags.includes(params.tag))
+
+  const pages = paginationRange(posts.length)
+  const page = INITIAL_PAGE_NUMBER
+  const slicedPosts = posts.slice((page - 1) * POSTS_PER_PAGE,
+                                  page * POSTS_PER_PAGE) // end is exclusive
+
+  return {
+    props: {
+      posts: slicedPosts,
+      pages,
+      tag: params.tag,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const allTags = Array.from(
+    getAllPosts([PostEntry.TAGS])
+      .map(e => new Set(e.tags))
+      .reduce((p, c) => p.union(c), new Set())
+  )
+
+  return {
+    paths: allTags.map((tag) => {
+      return {
+        params: {
+          tag,
+        },
+      }
+    }),
+    fallback: false,
+  }
+}
---
+++ pages/tag/[tag]/page/[page].tsx
@@ -0,0 +1,110 @@
+import Container from "@/components/container"
+import Layout from "@/components/layout"
+import ListPosts from "@/components/list-posts"
+import Pagination from "@/components/pagination"
+import TagTitle from "@/components/tag-title"
+import type Post from "@/interfaces/post"
+import { PostEntry } from "@/interfaces/post"
+import { getAllPosts, paginationRange } from "@/lib/api"
+import { BLOG_NAME, INITIAL_PAGE_NUMBER, POSTS_PER_PAGE } from "@/lib/constants"
+import Head from "next/head"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+
+type Props = {
+  posts: Post[]
+  pages: number[]
+  page: number
+  tag: string
+}
+
+export default function Page({ posts, pages, page, tag }: Props) {
+  const router = useRouter()
+  if (page == INITIAL_PAGE_NUMBER) {
+    useEffect(() => { router.replace(`/tag/${tag}/`) }, [])
+  }
+
+  const title = `${BLOG_NAME} | Tag: ${tag} | Page ${page}`
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>{title}</title>
+        </Head>
+        <Container>
+            <TagTitle tag={tag} />
+            {posts.length > 0 && <ListPosts posts={posts} />}
+            <Pagination
+              pages={pages}
+              currentPage={page}
+              basePath={`/tag/${tag}`}
+            />
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+type Params = {
+  params: {
+    tag: string
+    page: string
+  }
+}
+
+export async function getStaticProps({ params }: Params) {
+  const posts = getAllPosts([
+    PostEntry.TITLE,
+    PostEntry.DATES,
+    PostEntry.TAGS,
+    PostEntry.SLUG,
+    PostEntry.COVER_IMAGE,
+  ]).filter(post => post.tags.includes(params.tag))
+
+  const pages = paginationRange(posts.length)
+  const page = Number(params.page)
+  const slicedPosts = posts.slice((page - 1) * POSTS_PER_PAGE,
+                                  page * POSTS_PER_PAGE) // end is exclusive
+
+  return {
+    props: {
+      posts: slicedPosts,
+      pages,
+      page,
+      tag: params.tag,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const allPosts = getAllPosts([PostEntry.TAGS])
+  const allTags = Array.from(
+    allPosts
+      .map(e => new Set(e.tags))
+      .reduce((p, c) => p.union(c), new Set())
+  )
+
+  return {
+    paths: allTags.flatMap(tag => {
+      const tagPosts = allPosts.filter(e => e.tags.includes(tag)) // extract posts tagged the tag
+      const pageList = paginationRange(tagPosts.length)
+      return pageList.map(page => ({
+        params: {
+          tag,
+          page: page.toString(),
+        }
+      }))
+    }),
+    fallback: false,
+  }
+}
+
+
+// pageList.map((page) => {
+//   return {
+//     params: {
+//       tag:
+//       page: page.toString(),
+//     },
+//   }
+// })
